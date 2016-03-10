bc8de31be7e3af07818b0ce9a882ee3a9c26081f
Irakli <ir.mchedlishvili@gmail.com>
2016-03-10 20:50:46 +0400

    Script updated with proper comments and variables

--- scripts/inflation2datapackage.py
+++ scripts/inflation2datapackage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-# cpi2datapackage.py - convert WorldBank CPI source file to datapackage resource
-# Copyright (C) 2013  Tryggvi Björgvinsson
+# inflation2datapackage.py - convert WorldBank inflation source files to datapackage resource
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -21,20 +20,20 @@ import sys
 import csv
 import urllib
 
-# API URI for CPI information from The World Bank (in csv format)
+# API URL's for inflation information from The World Bank (in csv format)
 sources = ['http://api.worldbank.org/indicator/NY.GDP.DEFL.KD.ZG?format=csv', 'http://api.worldbank.org/indicator/NY.GDP.DEFL.KD.ZG?format=csv']
 file_names = ['inflation-consumer.csv', 'inflation-gdp.csv']
 
 def get_csv(source):
     """
-    Get the CPI data as a CSV. Returns a tuple where the first item
+    Get the inflation data as a CSV. Returns a tuple where the first item
     is the header row and the second item is the rows of the CSV.
     """
 
     # Get the CSV (use urllib.urlopen to allow files and urls)
-    cpi = urllib.urlopen(source)
+    inflation = urllib.urlopen(source)
     # Create the csv reader
-    csvreader = csv.reader(cpi)
+    csvreader = csv.reader(inflation)
     # Return a tuple: (headers, rows)
     return (csvreader.next(), csvreader)
 
@@ -42,9 +41,9 @@ def process(headers, rows):
     """
     Generator function to process a row in the source CSV and yield a row
     for the output csv. Rows in source CSV have country and country code in
-    the first two columns and then the rest of the columns hold the CPI for
-    each year (or no value if CPI isn't known). The output CSV will hold the
-    country, country code, the year, and the CPI value (so we're unwinding the
+    the first two columns and then the rest of the columns hold the inflation rate for
+    each year (or no value if inflation rate isn't known). The output CSV will hold the
+    country, country code, the year, and the inflation value (so we're unwinding the
     columns into rows)
     """
 
@@ -53,19 +52,19 @@ def process(headers, rows):
     yield ["Country", "Country Code", "Year", "Inflation"]
 
     for row in rows:
-        # Go through the CPI values in each row (we need the index as well)
-        for index, cpi in enumerate(row[2:]):
-            # If there is some value for the CPI we yield it
-            if cpi:
+        # Go through the inflation values in each row (we need the index as well)
+        for index, inflation in enumerate(row[2:]):
+            # If there is some value for the inflation we yield it
+            if inflation:
                 # We yield the country and the country code then we lookup
                 # the corresponding year in the header (we add 2 since we're
                 # enumerating from the third column)
-                yield row[:2]+[headers[index+2],cpi]
+                yield row[:2]+[headers[index+2], inflation]
 
 def write_csv(rows, filename=None):
     """
     Write rows to a CSV file. Use default dialect for the CSV. If a file name
-    is not provided, the rows will be printed to standard output
+    is not provided, but source is, the rows will be printed to standard output
     """
 
     # Set the file as stdout if no filename, else open the file for writing
@@ -81,12 +80,12 @@ if __name__ == "__main__":
     # Define arguments parser
     import argparse
     parser = argparse.ArgumentParser(
-        description='convert WorldBank CPI data to a data package resource')
+        description='convert WorldBank Inflation data to a data package resource')
     # Output file option
     parser.add_argument('-o', '--output', dest='filename', action='store',
                         default=None, metavar='filename',
                         help='define output filename')
-    # Source file (default is the global cpi_source)
+    # Source file (default is the global list of inflation URL's)
     parser.add_argument('source', default=sources, nargs='?',
                         help='source file to generate output from')
     # Parse the arguments into args
